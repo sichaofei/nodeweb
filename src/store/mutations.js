5f6afa0cd9963d68b0e278a53568ee754c4d28d6
Your Name <you@example.com>
2019-07-01 09:56:19 +0800

    12

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,4 @@
-// 提交 mutations是更改Vuex状态的唯一合法方法
-export const changeName = (state, name) => { // A组件点击更改餐馆名称为 A餐馆
-    state.name = name // 把方法传递过来的参数，赋值给state中的resturantName
-}
+
 export const changeUserId=(state,userId)=>{
     state.userId=userId
 }
